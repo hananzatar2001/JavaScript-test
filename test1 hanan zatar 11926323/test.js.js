c088891625df555a375222a2021a1c746fe63f4f
hananzatar2001 <90716074+hananzatar2001@users.noreply.github.com>
2021-10-19 23:59:40 +0300

    Add files via upload

---
+++ test1 hanan zatar 11926323/test.js.js
@@ -0,0 +1,11 @@
+
+var age =prompt('what is your age?');
+if(age<18)
+{
+    document.getElementById('li1').style.visibility="visible";
+}
+else
+{
+    document.getElementById('li2').style.visibility="visible";
+}
+
